49859be28b122bbe988660c9495aa88216a1af97
dIANE phan <dianephan12@gmail.com>
2021-03-16 19:06:04 -0700

    parsed out the file name from DB

--- app.py
+++ app.py
@@ -5,17 +5,31 @@ from sqlite3 import Error
 from werkzeug.datastructures import FileStorage
 load_dotenv()
 
+def insert_into_database(file_path_name, file_name_blob): 
+  try:
+    conn = sqlite3.connect('app.db')
+    conn.text_factory = str   
+    print("[INFO] : Successful connection!")
+    cur = conn.cursor()
+    insert_file = '''INSERT INTO uploads(file_names, file_blob)
+      VALUES(?, ?)'''
+    cur = conn.cursor()
+    cur.execute(insert_file, (file_path_name, file_name_blob, ))
+    conn.commit()
+    print("[INFO] : The blob for ", file_path_name, " is in the database.") 
+  except Error as e:
+    print(e)
+  finally:
+    if conn:
+      conn.close()
+    else:
+      error = "Oh shucks, something is wrong here."
+
 def convert_into_binary(file_path):
   print("[INFO] : converting into binary data rn")
   with open(file_path, 'rb') as file:
     binary = file.read()
   return binary
-
-def write_to_file(binary_data, file_name):
-  # dud_name = "converted_" + file_name
-  with open(file_name, 'wb') as file:
-    file.write(binary_data)
-  print("[DATA] : The following file has been written to file: ", file_name)
 
 def read_blob_data(entryID):
   try:
@@ -30,11 +44,10 @@ def read_blob_data(entryID):
       converted_file_name = row[1]
       photo_binarycode  = row[2]
       # parse out the file name from converted_file_name
-
-      # converted_file_name 
-      dud_file_name = "parsedfilenamehere" + ".png"
-
-      write_to_file(photo_binarycode, dud_file_name)
+      last_slash_index = converted_file_name.rfind("/") + 1 
+      png_index = converted_file_name.find('.png')
+      final_file_name = converted_file_name[last_slash_index:] 
+      write_to_file(photo_binarycode, final_file_name)
       print("[DATA] : Image successfully stored on disk. Check the project directory. \n")
     cur.close()
   except sqlite3.Error as error:
@@ -43,25 +56,10 @@ def read_blob_data(entryID):
     if conn:
         conn.close()
 
-def insert_into_database(file_path_name, file_name_blob): 
-  try:
-    conn = sqlite3.connect('app.db')
-    conn.text_factory = str   
-    print("[INFO] : Successful connection!")
-    cur = conn.cursor()
-    insert_file = '''INSERT INTO uploads(file_names, file_blob)
-      VALUES(?, ?)'''
-    cur = conn.cursor()
-    cur.execute(insert_file, (file_path_name, file_name_blob, ))
-    conn.commit()
-    print("[INFO] : the blob for ", file_path_name, " is in the database.") 
-  except Error as e:
-    print(e)
-  finally:
-    if conn:
-      conn.close()
-    else:
-      error = "Welp something is wrong here."
+def write_to_file(binary_data, file_name):
+  with open(file_name, 'wb') as file:
+    file.write(binary_data)
+  print("[DATA] : The following file has been written to file: ", file_name)
 
 def retrieve_file(entryID):
   try:
@@ -83,13 +81,10 @@ def retrieve_file(entryID):
       error = "how tf did u get here."
 
 def main():
-  print("Hello World!")
   file_path_name = raw_input("Enter full file path:\n") 
-  # file_path_name = "/Users/diane/Documents/DRAW_THE_OWL_MEME.png"
-  print("[DATA] : ", file_path_name) 
   file_name_blob = convert_into_binary(file_path_name)
   print("[INFO] : the last 100 characters of blob = ", file_name_blob[:100]) 
-  # insert_into_database(file_path_name, file_name_blob)
+  insert_into_database(file_path_name, file_name_blob)
   retrieve_file(1)
 
 if __name__ == "__main__":
